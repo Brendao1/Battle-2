d73229425045da13f6c803b5f75dc8573304d9d5
“Brenda” <odonnell.brenda@gmail.com>
2019-02-23 08:42:16 +0000

    Refactored out the global variable

--- app.rb
+++ app.rb
@@ -4,26 +4,31 @@ require_relative './lib/game'
 
 class Battle < Sinatra::Base
 
+  before do
+    @game = Game.instance
+  end
+
   get '/' do
     erb :index
   end
 
   post '/names' do
-    $game = Game.new(params[:Player1], params[:Player2])
+    # $game = Game.new(params[:Player1], params[:Player2])
+    @game = Game.create_game(params[:Player1], params[:Player2])
     redirect '/play'
   end
 
   get '/play' do
-    @Player1 = $game.Player1
-    @Player2 = $game.Player2
+    @Player1 = @game.Player1
+    @Player2 = @game.Player2
     erb :play
 
   end
 
 get '/attack' do
-    @Player1 = $game.Player1
-    @Player2 = $game.Player2
-    $game.attack
+    @Player1 = @game.Player1
+    @Player2 = @game.Player2
+    @game.attack
     erb :attack
 end
 
@@ -31,3 +36,5 @@ end
   # start the server if ruby file executed directly
   run! if app_file == $0
 end
+
+#adding comment for commit
--- lib/game.rb
+++ lib/game.rb
@@ -2,12 +2,20 @@ class Game
 
 attr_reader :Player1, :Player2, :turn, :attacker
 
+  def self.create_game(player_1, player_2)
+    @game = Game.new(player_1, player_2)
+  end
+
+  def self.instance
+    @game
+  end
+
   def initialize(user1, user2)
     @Player1 = Player.new(user1)
     @Player2 = Player.new(user2)
     @turn = @Player1
     @attacker = @Player2
-end
+  end
 
   def attack
     if @turn == @Player1
